804124271fd64bcedb1bf85582ed66fc0b7ea47c
RaphDou <raphael120701@gmail.com>
2023-11-13 20:09:25 -0500

    Update weatherScript.js

--- public/js/weatherScript.js
+++ public/js/weatherScript.js
@@ -1,20 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   fetch('/weather')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP! Statut : ${response.status}`);
+      }
+      return response.json();
+    })
     .then(data => {
       const weatherInfoElement = document.getElementById('weatherInfo');
-      const temperature = data.main.temp;
-      const description = data.weather[0].description;
-      const weatherEmoji = getWeatherEmoji(data.weather[0].main);
 
-      // Mise à jour du contenu de la balise li spécifique à la météo
-      weatherInfoElement.innerHTML = `Il fait actuellement ${temperature}°C à Québec. <span>${weatherEmoji}</span>`;
+      if (data && data.main && data.weather && data.weather[0]) {
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+        const weatherEmoji = getWeatherEmoji(data.weather[0].main);
+
+        // Mise à jour du contenu de la balise li spécifique à la météo
+        weatherInfoElement.innerHTML = `Il fait actuellement ${temperature}°C à Québec. <span>${weatherEmoji}</span>`;
+      } else {
+        console.error('Les données météorologiques ne sont pas dans le format attendu.', data);
+      }
     })
     .catch(error => {
       console.error('Erreur lors de la récupération des données météorologiques', error);
     });
 });
-
 
 // Fonction pour obtenir l'emoji en fonction de la condition météorologique principale
 function getWeatherEmoji(weatherMain) {
