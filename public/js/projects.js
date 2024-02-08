aea21703d1aa038ee26a52f2579e747986c885bf
RaphDou <raphael120701@gmail.com>
2024-02-08 00:24:03 -0500

    added project

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,50 +1,51 @@
 const projets = [
   {
+    id: 0,
+    titre: "Countries Explorer",
+    description: "Web application that allows users to explore information about countries using the REST Countries API",
+    image: "/images/earth.jpg",
+    technologies: ["Next.js", "React", "JavaScript"],
+    resultatsPersonnels: "This project allowed me to refine my skills in Next.js and REST API integrations.",
+    lien: "https://countries-explorer.onrender.com"
+  },
+  {
     id: 1,
     titre: "Memory Game",
     description: "JS programming project in duo to create a memory game.",
     image: "/images/cartes.jpg",
     technologies: ["JavaScript, HTML, CSS"],
-    resultatsPersonnels:
-      "First experience in JS programming and GitHub, not my best project lol!",
+    resultatsPersonnels: "First experience in JS programming and GitHub. (Currently redoing)",
   },
   {
     id: 2,
     titre: "SAPAR",
-    description:
-      "Final graphic design work done using Figma to create a fictional company.",
+    description: "Final graphic design work done using Figma to create a fictional company.",
     image: "/images/figma.jpg",
     technologies: ["Figma"],
-    resultatsPersonnels:
-      "Rewarding experience, showcasing creativity and interface design skills.",
+    resultatsPersonnels: "Rewarding experience, showcasing creativity and interface design skills.",
     lien: "https://www.figma.com/file/ZGtlHv40OP6SCxYY2iGwnd/TP3-Final?type=design&mode=design&t=hkWjkBlLcYGipNFx-1",
   },
   {
     id: 3,
     titre: "Contract",
-    description:
-      "A team work of three on a fictional project of a service offer contract.",
+    description: "A team work of three on a fictional project of a service offer contract.",
     image: "/images/contrat.jpg",
     technologies: ["Word"],
-    resultatsPersonnels:
-      "Development of key skills in communication, negotiation, and teamwork.",
+    resultatsPersonnels: "Development of key skills in communication, negotiation, and teamwork.",
   },
   {
     id: 4,
     titre: "Pokedex",
-    description:
-      "Next.js project with a link to an existing API to display a library of pokemon.",
+    description: "Next.js project with a link to an existing API to display a library of pokemon.",
     image: "/images/pokedex.jpg",
     technologies: ["Next.js"],
-    resultatsPersonnels:
-      "Good learning of Next.js, MUI library, and atomic design.",
+    resultatsPersonnels: "Good learning of Next.js, MUI library, and atomic design.",
     lien: "https://pokedex-x1ux.onrender.com",
   },
   {
     id: 5,
     titre: "Code Snippets",
-    description:
-      "A project to manage code snippets and personal notes attached to tags.",
+    description: "A project to manage code snippets and personal notes attached to tags.",
     image: "/images/snippets.png",
     technologies: ["JavaScript, HTML, CSS, node.js, express.js"],
     resultatsPersonnels: "First use of node.js and express!",
@@ -53,34 +54,28 @@ const projets = [
   {
     id: 6,
     titre: "Admin Panel",
-    description:
-      "Team project with an API to manage fictitious online product sales.",
+    description: "Team project with an API to manage fictitious online product sales.",
     image: "/images/ventes.jpg",
     technologies: ["Next.js"],
-    resultatsPersonnels:
-      "Good task management, time, and advanced functions with GitHub.",
+    resultatsPersonnels: "Good task management, time, and advanced functions with GitHub.",
     lien: "https://my-vendor-hub.onrender.com",
   },
   {
     id: 7,
     titre: "Company's API Development",
-    description:
-      "Our team worked full-time to develop a real company's API for building their website.",
+    description: "Our team worked full-time to develop a real company's API for building their website.",
     image: "/images/qc.jpg",
     technologies: ["JavaScript, Node.js, Express.js, MongoDB"],
-    resultatsPersonnels:
-      "Valuable experience in full-time project development, gaining insights into real-world website building.",
+    resultatsPersonnels: "Valuable experience in full-time project development, gaining insights into real-world website building.",
     lien: "https://documenter.getpostman.com/view/27391458/2s9YeBfZhu#c486b50d-042b-48e1-9fe2-f128def284a3",
   },
   {
     id: 8,
     titre: "WordPress shop",
-    description:
-      "Building a WordPress website using WooCommerce to create a working e-commerce shop.",
+    description: "Building a WordPress website using WooCommerce to create a working e-commerce shop.",
     image: "/images/wordpress.png",
     technologies: ["WordPress, WooCommerce"],
-    resultatsPersonnels:
-      "Significant learning in CMS website building and leveraging plugins for enhanced functionality.",
+    resultatsPersonnels: "Significant learning in CMS website building and leveraging plugins for enhanced functionality.",
     lien: "https://clientsmystere.com/Groupe5/",
   },
   {
